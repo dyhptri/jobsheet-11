9decdd1634cf27b38b57b3789e244af60257c2a1
dyhptri <hernandar0606@gmail.com>
2024-11-15 09:42:35 +0700

    modif percobaan 4

--- UcapanTerimaKasi07_modif.java
+++ UcapanTerimaKasi07_modif.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class UcapanTerimaKasih_07 {
+public class UcapanTerimaKasi07_modif {
     
     public static String PenerimaUcapan() {
         Scanner sc = new Scanner (System.in);
@@ -12,7 +12,12 @@ public class UcapanTerimaKasih_07 {
         String nama = PenerimaUcapan();
         System.out.println("Thank you " + nama + " for being the best teacher in the world.\n" + "You inspired in me a love for learning and me feel like i could ask you anything." );
     }
+    public static void UcapanTambahan (String ucapan){
+        System.out.println(ucapan);
+    }
     public static void main(String[] args) {
         UcapanTerimaKasih();
+        String ucapan = "Terima Kasih Semoga sehat selalu";
+        UcapanTambahan(ucapan);
     }
 }
